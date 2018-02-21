6f7a93efa149691bc358f0e4f59872d26cc51cb7
Tadej Stajner <tadej.tadej@gmail.com>
2018-02-21 14:18:36 +0100

    Add instapy to packages to install with setup.py

--- setup.py
+++ setup.py
@@ -20,5 +20,6 @@ setup(
     url='https://github.com/timgrossmann/InstaPy',
     py_modules='instapy',
     description=description,
-    install_requires=requirements
+    install_requires=requirements,
+    packages=['instapy']
 )
